d0417086e8339d6d5644256fe2e0594acfa83bfb
Reto Buerki <reet@codelabs.ch>
2014-05-23 17:12:09 +0200

    Add coverage support to GNAT engine

--- src/unit/engine/GNATtestEngine.php
+++ src/unit/engine/GNATtestEngine.php
@@ -5,6 +5,8 @@
  */
 final class GNATtestEngine extends ArcanistUnitTestEngine {
 
+  private $covCache = array();
+
   public function run() {
     $projectRoot = $this->getWorkingCopy()->getProjectRoot();
 
@@ -25,6 +27,75 @@ final class GNATtestEngine extends ArcanistUnitTestEngine {
       throw new Exception('git diff detected changes after test run');
     }
 
+    if ($this->getEnableCoverage() !== false) {
+      $this->addCoverage($results);
+    }
+
     return $results;
+  }
+
+  /**
+   * Read coverage report produced by gcov and add it to the test results.
+   * Inspired by MobileUnitTestEngine.php, thanks to the authors.
+   *
+   * @param array $results Unit test results
+   *
+   */
+  private function addCoverage($results) {
+
+    $cmd = 'find . -name *.gcda -exec gcov {} \; 2>&1';
+    exec($cmd, $output, $exit_status);
+
+    if ($exit_status > 0) {
+      throw new Exception('Command "'.$cmd.'" failed: '.implode($output));
+    }
+
+    foreach ($results as $result) {
+      $coverage = array();
+
+      $path = $result->getExtraData()[0];
+
+      if (!strlen($path)) {
+        continue;
+      }
+
+      if (array_key_exists($path, $this->covCache) !== false) {
+        $coverage[$path] = $this->covCache[$path];
+      } else {
+        $body = basename($path);
+        $gcov_filename = $body.'.gcov';
+        $str = '';
+
+        if (!file_exists($gcov_filename)) {
+          throw new Exception('Coverage file "'.$gcov_filename.'" does not exist');
+        }
+
+        foreach (file($gcov_filename) as $gcov_line) {
+          $gcov_matches = array();
+          if (preg_match('/.*?(\S|\d+):.*?(\d+)/is', $gcov_line, $gcov_matches)
+            && $gcov_matches[2][0] > 0
+          ) {
+            if ($gcov_matches[1][0] === '#' || $gcov_matches[1][0] === '=') {
+              $str .= 'U';
+            } else if ($gcov_matches[1][0] === '-') {
+              $str .= 'N';
+            } else if ($gcov_matches[1][0] > 0) {
+              $str .= 'C';
+            } else {
+              $str .= 'N';
+            }
+          }
+        }
+        $coverage[$path] = $str;
+        $this->covCache[$path] = $str;
+      }
+      $result->setCoverage($coverage);
+    }
+
+    foreach (glob('*.gcov') as $file) {
+      unlink($file);
+    }
+
+    return $coverage;
   }
 }

940a0443cdd154058f71977c3009db00d89d66b8
Reto Buerki <reet@codelabs.ch>
2024-11-20 15:35:42 +0100

    Fixup "Remove lint and unit excuses ..."
    Adjust reverted, older code to avoid deprecated warnings.

--- src/workflow/ArcanistDiffWorkflow.php
+++ src/workflow/ArcanistDiffWorkflow.php
@@ -2389,7 +2389,7 @@ EOTEXT
    * @task diffprop
    */
   private function updateLintDiffProperty() {
-    if (strlen($this->excuses['lint'])) {
+    if (phutil_nonempty_string($this->excuses['lint'])) {
       $this->updateDiffProperty(
         'arc:lint-excuse',
         json_encode($this->excuses['lint']));
@@ -2413,7 +2413,7 @@ EOTEXT
    * @task diffprop
    */
   private function updateUnitDiffProperty() {
-    if (strlen($this->excuses['unit'])) {
+    if (phutil_nonempty_string($this->excuses['unit'])) {
       $this->updateDiffProperty('arc:unit-excuse',
         json_encode($this->excuses['unit']));
     }
